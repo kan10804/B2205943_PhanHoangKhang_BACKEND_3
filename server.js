789773c4de9ea149053694d7cd078acd155462b6
PhanHoangKhang <phkhang1088@gmail.com>
2025-10-20 09:29:38 +0700

    Khoi tao du an

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
+async function startServer() {
+  try {
+    // Kết nối MongoDB
+    await MongoDB.connect(config.db.uri);
+    console.log("Connected to the database!");
 
-// start server
-const POST = config.app.port;
-app.listen(POST, () => {
-    console.log('Server is running on port ${POST}.');
-});
+    // Khởi chạy server
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Cannot connect to the database!", error);
+    process.exit();
+  }
+}
+
+startServer();
