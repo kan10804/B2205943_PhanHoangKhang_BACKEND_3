6cb6349bc3e89790e72cbd66a74a9211e9507f71
PhanHoangKhang <phkhang1088@gmail.com>
2025-11-03 15:03:40 +0700

    Cap nhat ContactAdd và ContactBook

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -119,8 +119,8 @@ export default {
       }
     },
     goToAddContact() {
-      this.$router.push({ name: "contact.add" });
-    },
+  this.$router.push({ name: "contact.add" });
+}
   },
   mounted() {
     this.refreshList();
